9adb3ec54199a431ef2c377d7b6670a83d37fa8c
Jadurani Davalos <jadurani.davalos@gmail.com>
2019-12-10 17:28:33 +0800

    Working: CF to copy created user to CMS MySQL DB

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const fetch = require('node-fetch');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const encodeBody = (params) => Object.keys(params).map((key) => {
+  return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+}).join('&');
+
+exports.createCMSUser = functions.auth.user().onCreate(async user => {
+  const url = 'https://app.checkmyschool.org/api-2/cmsappuser/create_user';
+  const userDataJson = {
+    firebaseId: user.uid,
+    email: user.email
+  };
+
+  if (user.displayName) {
+    data['fullName'] = user.displayName;
+  }
+
+  if (user.photoURL) {
+    data['photoUrl'] = user.photoUrl;
+  }
+
+  const data = encodeBody(userDataJson);
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    referrer: 'no-referrer',
+    body: data
+  });
+
+  return await response.json();
+});
